c329724d1c2209dcbb1b034a9e0a7cab3e19cfa9
souravkunduss1@outlook.com <souravkunduss1@outlook.com>
2024-10-02 00:05:33 +0600

    Viewmodels, DBInitializers, Roles added and other minor changes done

--- Shortly.Client/Controllers/AuthenticationController.cs
+++ Shortly.Client/Controllers/AuthenticationController.cs
@@ -1,19 +1,98 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualStudio.Web.CodeGenerators.Mvc.Templates.BlazorIdentity.Pages;
+using Shortly.Client.Data.ViewModels;
+using Shortly.Data.Models;
+using Shortly.Data.Services;
 
 namespace Shortly.Client.Controllers
 {
     public class AuthenticationController : Controller
     {
-        public IActionResult Users()
-        {
-            return View();
-        }
+        private IUsersService _usersService;
+        private SignInManager<AppUser> _signInManager;
+        private UserManager<AppUser> _userManager;
+        private IConfiguration _configuration;
 
-        public IActionResult Login()
+        public AuthenticationController(IUsersService usersService,
+             SignInManager<AppUser> signInManager,
+             UserManager<AppUser> userManager,
+             IConfiguration configuration)
         {
-            return View();
-        }
+            _usersService = usersService;
+            _signInManager = signInManager;
+            _userManager = userManager;
+            _configuration = configuration;
 
+        }
+        public async Task<IActionResult> Users()
+        {
+            var users = await _usersService.GetUsersAsync();
+            return View(users);
+        }
+        
+        public async Task<IActionResult> Login()
+        {
+            /* var loginVM = new LoginVM()
+             {
+                 Schemes = await _signInManager.GetExternalAuthenticationSchemesAsync()
+             };*/
+
+            return View(new LoginVM());
+        }
+        
+        public async Task<IActionResult> LoginSubmitted(LoginVM loginVM)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("Login", loginVM);
+            }
+
+            var user = await _userManager.FindByEmailAsync(loginVM.EmailAddress);
+            if (user != null)
+            {
+                var userPasswordCheck = await _userManager.CheckPasswordAsync(user, loginVM.Password);
+                if (userPasswordCheck)
+                {
+                    var userLoggedIn = await _signInManager.PasswordSignInAsync(user, loginVM.Password, false, false);
+
+                    if (userLoggedIn.Succeeded)
+                    {
+                        return RedirectToAction("Index", "Home");
+                    }
+                    else if (userLoggedIn.IsNotAllowed)
+                    {
+                        return RedirectToAction("EmailConfirmation");
+                    }
+                    else if (userLoggedIn.RequiresTwoFactor)
+                    {
+                        return RedirectToAction("TwoFactorConfirmation", new { loggedInUserId = user.Id });
+                    }
+
+                    else
+                    {
+                        ModelState.AddModelError("", "Invalid login attempt. Please, check your username and password");
+                        return View("Login", loginVM);
+                    }
+                }
+                else
+                {
+                    await _userManager.AccessFailedAsync(user);
+
+                    if (await _userManager.IsLockedOutAsync(user))
+                    {
+                        ModelState.AddModelError("", "Your account is locked, please try again in 10 mins");
+                        return View("Login", loginVM);
+                    }
+
+                    ModelState.AddModelError("", "Invalid login attempt. Please, check your username and password");
+                    return View("Login", loginVM);
+                }
+            }
+
+
+            return RedirectToAction("Index", "Home");
+        }
 
         public IActionResult Register()
         {
--- Shortly.Client/Program.cs
+++ Shortly.Client/Program.cs
@@ -3,9 +3,13 @@ using Shortly.Client.Data;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Shortly.Data;
+using Shortly.Data.Services;
 
 
 var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
 
 //Configure the AppDbContext
 builder.Services.AddDbContext<AppDbContext>(options =>
@@ -13,10 +17,58 @@ builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
+//Configure Authentication
+//1. Add identity service
+builder.Services.AddIdentity<AppUser, IdentityRole>()
+    .AddEntityFrameworkStores<AppDbContext>()
+    .AddDefaultTokenProviders();
 
 
-// Add services to the container.
-builder.Services.AddControllersWithViews();
+//2. Configure the application cookie
+builder.Services.ConfigureApplicationCookie(options =>
+{
+    options.Cookie.HttpOnly = true;
+    options.ExpireTimeSpan = TimeSpan.FromMinutes(60);
+    options.LoginPath = "/Authentication/Login";
+    options.SlidingExpiration = true;
+});
+
+//3. Update the default password settings
+builder.Services.Configure<IdentityOptions>(options =>
+{
+    //Password settings
+    options.Password.RequireDigit = false;
+    options.Password.RequireLowercase = false;
+    options.Password.RequireUppercase = false;
+    options.Password.RequireNonAlphanumeric = false;
+    options.Password.RequiredLength = 6;
+
+    //Lockout settings
+    options.Lockout.MaxFailedAccessAttempts = 5;
+    options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(10);
+
+    //Signin settings
+    options.SignIn.RequireConfirmedEmail = true;
+});
+
+
+//Add services to the container
+builder.Services.AddScoped<IUrlsService, UrlsService>();
+builder.Services.AddScoped<IUsersService, UsersService>();
+
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+/*builder.Services.AddAuthentication()
+    .AddGoogle(options =>
+    {
+        options.ClientId = builder.Configuration["Auth:Google:ClientId"];
+        options.ClientSecret = builder.Configuration["Auth:Google:ClientSecret"];
+    })
+    .AddGithub(options =>
+    {
+        options.ClientId = builder.Configuration["Auth:GitHub:ClientId"];
+        options.ClientSecret = builder.Configuration["Auth:GitHub:ClientSecret"];
+    });*/
 
 var app = builder.Build();
 
@@ -41,5 +93,8 @@ app.UseAuthorization();
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
+
+//Seed the database
+DbInitializer.SeedDefaultUsersAndRolesAsync(app).Wait();
 
 app.Run();
